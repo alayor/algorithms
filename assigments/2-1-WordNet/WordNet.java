a783edd90e300b2a9b5a63d80e9731130d040973
Alonso Ayala <alayor3@gmail.com>
2015-11-07 18:53:22 +0000

    Adding distance and sap methods to WordNet.

--- assigments/2-1-WordNet/WordNet.java
+++ assigments/2-1-WordNet/WordNet.java
@@ -5,9 +5,12 @@ import edu.princeton.cs.algs4.Digraph;
 public class WordNet {
     private final int ID_INDEX = 0;
     private final int SYNSET_INDEX = 1;
-    Digraph _digraph;
-    Map<String, List<Integer>> _nounSynsets = new HashMap<String, List<Integer>>();
-    int _synsetsCount;
+    private Digraph _digraph;
+    private final Map<String, List<Integer>> _nounSynsets = new HashMap<String, List<Integer>>();
+    private final Map<Integer, String> _synsets = new HashMap<Integer, String>();
+    private final Map<NounPair, Integer> nounPairLength = new HashMap<NounPair, Integer>();
+    private final Map<NounPair, String> nounPairAncestor= new HashMap<NounPair, String>();
+    private int _synsetsCount;
 
     public WordNet(String synsets, String hypernyms) {
         In synsetsFile = new In(synsets);
@@ -26,7 +29,9 @@ public class WordNet {
 
     private void addSynsetIdToNoun(String[] row) {
         Integer synsetId = Integer.parseInt(row[ID_INDEX]);
-        String[] nouns = row[SYNSET_INDEX].split("\\s+");
+        String synsetName = row[SYNSET_INDEX];
+        _synsets.put(synsetId, synsetName);
+        String[] nouns = synsetName.split("\\s+");
         for (String noun : nouns) {
             List<Integer> synsets = _nounSynsets.get(noun);
             if(synsets == null) synsets = new ArrayList<Integer>();
@@ -39,7 +44,12 @@ public class WordNet {
         _digraph = new Digraph(_synsetsCount);
         while(hypernymsFile.hasNextLine()) {
             String[] arr = hypernymsFile.readLine().split(",");
-            _digraph.addEdge(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]));
+            if (arr.length > 1) {
+                String[] hypernyms = arr[1].split(",");
+                for (String hypernym : hypernyms) {
+                    _digraph.addEdge(Integer.parseInt(arr[0]), Integer.parseInt(hypernym));
+                }
+            }
         }
     }
 
@@ -52,8 +62,88 @@ public class WordNet {
         };
     }
 
+    private String getSynsetName(int synsetId) {
+        return _synsets.get(synsetId);
+    }
+
+    private List<Integer> getSynsetsForNoun(String noun) {
+        return _nounSynsets.get(noun);
+    }
+
     public boolean isNoun(String word) {
         return _nounSynsets.containsKey(word);
+    }
+
+    public int distance(String nounA, String nounB) {
+        NounPair nounPair = new NounPair(nounA, nounB);
+        Integer minDistance = nounPairLength.get(nounPair);
+        List<Integer> synsetsA = _nounSynsets.get(nounA);
+        List<Integer> synsetsB = _nounSynsets.get(nounB);
+        if (synsetsA == null || synsetsB == null) {
+            return -1;
+        }
+        if (minDistance == null) {
+            minDistance = Integer.MAX_VALUE;
+            SAP sap = new SAP(_digraph);
+            int ancestor = -1;
+            for (Integer synA : synsetsA) {
+                for (Integer synB : synsetsB) {
+                    int distance = sap.length(synA, synB);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        ancestor = sap.ancestor(synA, synB);
+                    }
+                }
+            }
+            nounPairLength.put(nounPair, minDistance);
+            nounPairAncestor.put(nounPair, getSynsetName(ancestor));
+        }
+        return minDistance;
+    }
+
+    public String sap(String nounA, String nounB) {
+        NounPair nounPair = new NounPair(nounA, nounB);
+        String ancestorName = nounPairAncestor.get(nounPair);
+        List<Integer> synsetsA = _nounSynsets.get(nounA);
+        List<Integer> synsetsB = _nounSynsets.get(nounB);
+        if (synsetsA == null || synsetsB == null) {
+            return "";
+        }
+        if (ancestorName == null) {
+            Integer minDistance = Integer.MAX_VALUE;
+            SAP sap = new SAP(_digraph);
+            int ancestor = -1;
+            for (Integer synA : synsetsA) {
+                for (Integer synB : synsetsB) {
+                    int distance = sap.length(synA, synB);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        ancestor = sap.ancestor(synA, synB);
+                    }
+                }
+            }
+            ancestorName = getSynsetName(ancestor);
+            nounPairLength.put(nounPair, minDistance);
+            nounPairAncestor.put(nounPair, ancestorName);
+        }
+        return ancestorName;
+    }
+
+    private class NounPair {
+        private final String nounA;
+        private final String nounB;
+
+        NounPair(String nounA, String nounB) {
+            this.nounA = nounA;
+            this.nounB = nounB;
+        }
+
+        public boolean equals(Object o) {
+            if (o == null) return false;
+            if (o instanceof NounPair == false) return false;
+            NounPair that = (NounPair) o;
+            return this.nounA.equals(that.nounA) && this.nounB.equals(that.nounB);
+        }
     }
 
     public static void main(String[] args) {
--- assigments/2-1-WordNet/WordNetTest.java
+++ assigments/2-1-WordNet/WordNetTest.java
@@ -5,6 +5,8 @@ public class WordNetTest {
     public static void runTests() {
         testNouns();
         testIsNoun();
+        testDistance();
+        testAncestor();
     }
 
     private static void testNouns() {
@@ -14,7 +16,7 @@ public class WordNetTest {
         while(iterator.hasNext()) {
             nouns.add(iterator.next());
         }
-        assert nouns.size() == 4;
+        assert nouns.size() == 5;
         assert nouns.contains("'hood");
         assert nouns.contains("hola");
         assert nouns.contains("15_May_Organization");
@@ -27,6 +29,19 @@ public class WordNetTest {
         assert wordNet.isNoun("hola");
         assert wordNet.isNoun("15_May_Organization");
         assert wordNet.isNoun("'hood");
+        assert wordNet.isNoun("Alonso");
         assert wordNet.isNoun("1750ss") == false;
+    }
+
+    private static void testDistance() {
+        WordNet wordNet = new WordNet("wordnet/synsets.txt", "wordnet/hypernyms.txt");
+        double distance = wordNet.distance("worm", "bird");
+        assert distance == 12 : distance;
+    }
+
+    private static void testAncestor() {
+        WordNet wordNet = new WordNet("wordnet/synsets.txt", "wordnet/hypernyms.txt");
+        String ancestor = wordNet.sap("worm", "bird");
+        assert ancestor.equals("physical_entity") : ancestor;
     }
 }
